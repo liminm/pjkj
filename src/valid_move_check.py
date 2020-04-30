fc40a4018e7a18167c0b9dd709edda9b65c8a02a
Sebastian Piotr Polak <s.polak@campus.tu-berlin.de>
2020-04-30 17:10:24 +0200

    Change in-& output of 'calc_positions'
    Many changes to suit the requirements.
    
    New: get_figure

--- src/valid_move_check.py
+++ src/valid_move_check.py
@@ -2,10 +2,52 @@ from bitboard import racing_kings
 
 
 class ValidCheck():
-    def get_position(self, bitboard):
+    # get the figure that was moved
+    # return "" if:
+    #               no figure moved
+    #               too many moves
+    def get_figure(self, before, after):
+        if after.player == 'w':
+            return self.get_figure_w(before, after, 0)
+        elif after.player == "b":
+            return self.get_figure_b(before, after, 0)
+        else:
+            return ""
+
+    def get_figure_w(self, before, after, fig_moved):
+        curr_fig = ""
+        for fig in "KBNRQ":
+            try:
+                fig_board_before = before.white_board[fig]
+                fig_board_after  = after.white_board[fig]
+            except KeyError:
+                continue
+            if fig_board_before != fig_board_after:
+                curr_fig = fig
+                fig_moved += 1
+        if (fig_moved == 1):
+            return curr_fig
+        return ""
+
+    def get_figure_b(self, before, after, fig_moved):
+        curr_fig = ""
+        for fig in "kbrnq":
+            try:
+                fig_board_before = before.black_board[fig]
+                fig_board_after  = after.black_board[fig]
+            except KeyError:
+                continue
+            if fig_board_before != fig_board_after:
+                curr_fig = fig
+                fig_moved += 1
+        if (fig_moved == 1):
+            return curr_fig
+        return ""
+
+    def get_position(self, bitboard): #todo fix?
         x = -1
         y = 0
-        if bitboard == 0: # todo: check if only one bit is set (only one figure moves)
+        if bitboard == 0:
             return (-1, -1)
         while bitboard != 0:
             bitboard = bitboard >> 1
@@ -15,43 +57,65 @@ class ValidCheck():
                 y += 1
         return (x, y)
 
-    def get_direction(self, board_before, board_after):
-        mix = board_before & board_after
-        board_before = board_before - mix
-        board_after  = board_after  - mix
+    def calc_positions(self, board_before, board_after):
+        before = racing_kings()
+        after  = racing_kings()
 
-        start_pos = self.get_position(board_before)
-        end_pos   = self.get_position(board_after)
+        before.toBitBoard(board_before)
+        after.toBitBoard(board_after)
 
-        if start_pos == (-1,-1) or end_pos == (-1,-1):
-            return (0, 0)
-        x_dir = end_pos[0] - start_pos[0]
-        y_dir = end_pos[1] - start_pos[1]
-        return (x_dir, y_dir)
+        figure = self.get_figure(before, after)
+        if figure == "":
+            return figure, (0,0) ,(0,0)
+
+        if after.player == "w":
+            bit_before = before.white_board[figure]
+            bit_after = after.white_board[figure]
+        else:
+            bit_before = before.black_board[figure]
+            bit_after = after.black_board[figure]
+
+        #print("before: ","{0:b}".format(bit_before))
+        #print("after:  ","{0:b}".format(bit_after))
+
+        mix = bit_before & bit_after # todo check alternative (doesnt work?)
+        bit_before = bit_before - mix
+        bit_after  = bit_after  - mix
+
+        #print("mix:    ","{0:b}".format(mix))
+
+        ##todo: check if only one figure moves
+
+        start_pos = self.get_position(bit_before)
+        end_pos   = self.get_position(bit_after)
+
+        return figure, start_pos, end_pos
 
 
-# print("{0:b}".format(test))
-game  = racing_kings()
 check = ValidCheck()
-#board = "r7/8/8/8/8/8/8/7r w"
-#game.toBitBoard(board)
-#state = game.cur_state
-#game.printBoard(game.black_board['r'])
-#print(game.black_board)
 
-a1 = 0b00110000000010
-a2 = 0b10110000000000
+#board1 = "r7/8/8/8/1q7/8/8/r7 w - - 3 2" # produces wrong board -> overflow?
+#board2 = "1r6/8/8/8/1q7/8/8/r7 b - - 4 5"
+
+board1 = "2r5/8/8/8/1q7/8/8/r7 w - - 3 2"
+board2 = "1r6/8/8/8/1q7/8/8/r7 b - - 4 5"
+
+move = check.calc_positions(board1, board2)
+print(move)
+# game.toBitBoard(board1)
+# state = game.cur_state
+# game.printBoard(game.black_board['r'])
+# game.printBoard(state)
+# print(game.cur_state)
+
+# example
+# a1 = 0b00110000000010
+# a2 = 0b10110000000000
 # print("{0:b}".format(a1))
 # print("{0:b}".format(a2))
-b = a1 & a2
-b1 = a1 - b
-b2 = a2 - b
 
-print("start position: ", check.get_position(b1))
-print("destination:    ", check.get_position(b2))
-
-move = check.get_direction(a1,a2)
-print("move (x,y):     ",move)
+# move = check.calc_positions(a1, a2)
+# print("Figure, (x,y):     ",move)
 
 # TODO: check if only one figure moves
 # TODO: check if figure is not out of bounds
@@ -61,3 +125,5 @@ print("move (x,y):     ",move)
 # print(game.cur_state)
 # before = "8/8/8/1K6/8/8/8/8 w"
 # after  = "8/8/8/2K5/8/8/8/8 w"
+
+# print("{0:b}".format(test))
