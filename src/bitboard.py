3bbf8988fa20624880033c85f83a1b45bf7e198e
Sebastian Piotr Polak <s.polak@campus.tu-berlin.de>
2020-05-01 13:46:58 +0200

    Add comments, and more functionality
    New:
    Comments
    Check for one-figure-move
    count_bits of a number
    
    Rename untitled -> bitboard

--- src/bitboard.py
+++ src/bitboard.py
@@ -23,30 +23,30 @@ class racing_kings():
                 'k': 0<<63,
                 'b': 0<<63,
                 'n': 0<<63,
-                'r': 0 << 63,
+                'r': 0<<63,
                 'q': 0<<63,
                 }
         self.white_board = {
-                'K': 0 << 63,
+                'K': 0<<63,
                 'B': 0<<63,
-                'N': 0 << 63,
+                'N': 0<<63,
                 'R': 0<<63,
-                'Q': 0 << 63,
+                'Q': 0<<63,
                 }
         self.cur_state = 0<<63
         self.black_board_last = {
                 'k': 0<<63,
                 'b': 0<<63,
                 'n': 0<<63,
-                'r': 0 << 63,
+                'r': 0<<63,
                 'q': 0<<63,
                 }
         self.white_board_last = {
-                'K': 0 << 63,
+                'K': 0<<63,
                 'B': 0<<63,
-                'N': 0 << 63,
+                'N': 0<<63,
                 'R': 0<<63,
-                'Q': 0 << 63,
+                'Q': 0<<63,
                 }
         self.last_state = 0<<63
         
@@ -72,7 +72,9 @@ class racing_kings():
         self.white_board_last = self.white_board.copy()
       
         self.resetBoard()
-        
+
+        self.player = turn_parts[1]
+
         for elem in turn_parts[0]:
             if ord(elem) < 57 and ord(elem) > 48:  #we have a number with 1-8 49-56
                 pos += int(elem)
@@ -129,52 +131,18 @@ class racing_kings():
         self.black_board.clear()
         self.white_board.clear()
         self.cur_state = 0<< 63
-        
-    def moved(self):
-        """
-        Goal is to return the figure k,r,n,b,q,K,R,N,B or Q and field number i.e. r-63-60
-        with 63 being the start field and 60 the end field.
-        """
-        ret = ''
-        w = []
-        b = []
-        keys = ['k','b','r','n','q']
-        move = []
-        for x in keys:
-            y = x.upper()
-            if ((self.white_board_last[y] & (-self.white_board[y] -1)) != 0 ):
-                move = str(self.white_board_last[y] & self.white_board[y]).split()
-                print(move)
-                w.append(y)    
-           # print(bool(self.black_board_last[x] & self.black_board[x]), "for", self.black_board_last[x], "and", self.black_board[x])
-            if ((self.black_board_last[x] & (-self.black_board[x] -1 )) != 0):
-                print("whiole :", '{0:b}'.format(self.black_board_last[x] ^ self.black_board[x]))
-                move = str(self.black_board_last[x] ^ self.black_board[x]).split('1')
-                for i in range(len(move)):
-                    parts = move[i]
-                    length = len(move[i])
-                for i in parts:
-                    val = int(i)+
-                    ret = ret +x+' '+int(i)+
-                b.append(x)
-        print(b)
-        print(w)
-        return ret
-    
-    
-        
-                
-            
-game = racing_kings()
-game.printBoard(game.cur_state)
+
+
+
+#game = racing_kings()
+#game.printBoard(game.cur_state)
 #print('{0:b}'.format(game.black_board['r']))
 #print(game.black_board['r'])
-game.toBitBoard("8/8/8/8/8/r7/k1bnKRBN/qrbnQRBN w - - 0 1")
+#game.toBitBoard("8/8/8/8/8/r7/k1bnKRBN/qrbnQRBN w - - 0 1")
 #game.printBoard(game.cur_state)
 #game.printBoard(game.last_state)
 #print(game.black_board_last['r'])
 #print('{0:b}'.format(game.black_board['r']))
-game.moved()
-test = "8/8/8/8/8/r7/k1bnKRBN/qrbnQRBN w - - 0 1"
-parts = test.split()
+#test = "8/8/8/8/8/r7/k1bnKRBN/qrbnQRBN w - - 0 1"
+#parts = test.split()
 
--- src/valid_move_check.py
+++ src/valid_move_check.py
@@ -2,10 +2,52 @@ from bitboard import racing_kings
 
 
 class ValidCheck():
+
+    # Calcualtes the position of the moved figure
+    # @board_before: FEN-board before the move
+    # @board_after:  FEN-board after the move
+    # @return: figure-type, before-position, after-position    example: 'r', (1,0), (2,3)
+    def calc_positions(self, board_before, board_after):
+        before = racing_kings()
+        after  = racing_kings()
+
+        before.toBitBoard(board_before)
+        after.toBitBoard(board_after)
+
+        figure = self.get_figure(before, after)
+        if figure == "":
+            return figure, (-1,-1) ,(-1,-1)
+
+        if after.player == "w":
+            bit_before = before.white_board[figure]
+            bit_after = after.white_board[figure]
+        else:
+            bit_before = before.black_board[figure]
+            bit_after = after.black_board[figure]
+
+        #print("before: ","{0:b}".format(bit_before))
+        #print("after:  ","{0:b}".format(bit_after))
+
+        mix = bit_before & bit_after
+        bit_before = bit_before - mix
+        bit_after  = bit_after  - mix
+
+        #print("mix:    ","{0:b}".format(mix))
+
+        # check if only one figure moves
+        if (self.count_bits(bit_before) > 1) or (self.count_bits(bit_after) > 1):
+            return "", (-1, -1), (-1, -1)
+
+        # todo?: check if figure of other player wasn't moved
+
+        start_pos = self.get_position(bit_before)
+        end_pos   = self.get_position(bit_after)
+
+        return figure, start_pos, end_pos
+
     # get the figure that was moved
-    # return "" if:
-    #               no figure moved
-    #               too many moves
+    # return "" if: - no figure moved
+    #               - too many moves
     def get_figure(self, before, after):
         if after.player == 'w':
             return self.get_figure_w(before, after, 0)
@@ -20,7 +62,7 @@ class ValidCheck():
             try:
                 fig_board_before = before.white_board[fig]
                 fig_board_after  = after.white_board[fig]
-            except KeyError:
+            except KeyError: # catch Error, if such figure doesn't exist
                 continue
             if fig_board_before != fig_board_after:
                 curr_fig = fig
@@ -44,7 +86,9 @@ class ValidCheck():
             return curr_fig
         return ""
 
-    def get_position(self, bitboard): #todo fix?
+    # req: only one bit set (one figure on bitboard)
+    # @return: position of the figure on the bitboard
+    def get_position(self, bitboard):
         x = -1
         y = 0
         if bitboard == 0:
@@ -57,48 +101,24 @@ class ValidCheck():
                 y += 1
         return (x, y)
 
-    def calc_positions(self, board_before, board_after):
-        before = racing_kings()
-        after  = racing_kings()
-
-        before.toBitBoard(board_before)
-        after.toBitBoard(board_after)
-
-        figure = self.get_figure(before, after)
-        if figure == "":
-            return figure, (0,0) ,(0,0)
-
-        if after.player == "w":
-            bit_before = before.white_board[figure]
-            bit_after = after.white_board[figure]
-        else:
-            bit_before = before.black_board[figure]
-            bit_after = after.black_board[figure]
-
-        #print("before: ","{0:b}".format(bit_before))
-        #print("after:  ","{0:b}".format(bit_after))
-
-        mix = bit_before & bit_after # todo check alternative (doesnt work?)
-        bit_before = bit_before - mix
-        bit_after  = bit_after  - mix
-
-        #print("mix:    ","{0:b}".format(mix))
-
-        ##todo: check if only one figure moves
-
-        start_pos = self.get_position(bit_before)
-        end_pos   = self.get_position(bit_after)
-
-        return figure, start_pos, end_pos
-
+    def count_bits(self, number):
+        i = 0
+        while (number > 0):
+            i += number & 1
+            number = number >> 1
+        return i
 
 check = ValidCheck()
 
 #board1 = "r7/8/8/8/1q7/8/8/r7 w - - 3 2" # produces wrong board -> overflow?
 #board2 = "1r6/8/8/8/1q7/8/8/r7 b - - 4 5"
 
-board1 = "2r5/8/8/8/1q7/8/8/r7 w - - 3 2"
-board2 = "1r6/8/8/8/1q7/8/8/r7 b - - 4 5"
+board1 = "r7/8/8/8/1q7/8/8/8 w - - 3 2"
+board2 = "2r5/8/8/8/1q7/8/8/8 b - - 4 5"
+
+# game = racing_kings()
+# game.toBitBoard(board1)
+# game.printBoard(game.black_board['r'])
 
 move = check.calc_positions(board1, board2)
 print(move)
@@ -118,9 +138,9 @@ print(move)
 # print("Figure, (x,y):     ",move)
 
 # TODO: check if only one figure moves
-# TODO: check if figure is not out of bounds
-# TODO: check if the translation is right (direction, board orientation)
-# TODO: add actual check
+# TODO: check if figure is not out of bounds -> happens before the bitboard-conversion
+# TODO: check if the translation is right (direction, board orientation) -> irrelevant
+# TODO: add actual check ->HeyiLee
 
 # print(game.cur_state)
 # before = "8/8/8/1K6/8/8/8/8 w"
