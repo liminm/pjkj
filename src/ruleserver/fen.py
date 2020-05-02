db42d1e4fdbde2a1fbe1de1d5cc2f38d003581fd
Tobiasloch <Tobias.loch@tu-berlin.de>
2020-05-02 21:55:34 +0200

    - fixed the board representation, because black and bishop and knight and king had the same identifier - implemented a __repr__ function for the bitboard which will print the actual bitboard as a string

--- src/ruleserver/fen.py
+++ src/ruleserver/fen.py
@@ -21,14 +21,14 @@ class Board:
         self.start = "8/8/8/8/8/8/krbnKRBN/qrbnQRBN w - - 0 1"
         self.string = string
         self.board = {
-            "q": np.int64(0),
-            "k": np.int64(0),
-            "p": np.int64(0),
-            "b": np.int64(0),
-            "k": np.int64(0),
-            "r": np.int64(0),
-            "w": np.int64(0),
-            "b": np.int64(0)
+            "q": np.uint64(0), # queen
+            "k": np.uint64(0), # king
+            "p": np.uint64(0), # pawn
+            "b": np.uint64(0), # bishop
+            "n": np.uint64(0), # knight
+            "r": np.uint64(0), # rook
+            "wh": np.uint64(0), # white
+            "bl": np.uint64(0) # black
         }
         
         self.pattern = re.compile("([1-8rnbqkpPRNBQK]{1,8})\/([1-8rnbqkpPRNBQK]{1,8})\/([1-8rnbqkpPRNBQK]{1,8})\/([1-8rnbqkpPRNBQK]{1,8})\/([1-8rnbqkpPRNBQK]{1,8})\/([1-8rnbqkpPRNBQK]{1,8})\/([1-8rnbqkpPRNBQK]{1,8})\/([1-8rnbqkpPRNBQK]{1,8}) ([wb]) (\-|KQ?k?q?|K?Qk?q?|K?Q?kq?|K?Q?k?q) (\-|[a-f][1-8]) (\d+) (\d+)")
@@ -84,21 +84,7 @@ class Board:
         
         # fen ist ab hier eine liste, im im kommentar über der scan funktion beschriebenend format
         # du kannst auch python fen.py ausführen, dann siehst du einen beispielaufruf von fen
-           
-    def to_matrix(self, board_str):
-        self.vector = []
-        self.matrix = []
-        for char in board_str:
-            if char.isdigit():
-                for i in range(int(char)):
-                    self.vector.append(" ")
-            elif char == "/":
-                self.matrix.append(self.vector)
-                self.vector = []
-            else:
-                self.vector.append(char)
-        self.matrix.append(self.vector)
-        return self.matrix
+        pass
            
     def printBoard(self, bitboard):
         board = '{0:b}'.format(bitboard).zfill(64)
@@ -126,13 +112,54 @@ class Board:
                 self.vector.append(char)
         self.matrix.append(self.vector)
         return self.matrix
+        
+    def __repr__(self):
+        get_bin = lambda x, n: format(x, 'b').zfill(n)
+        toBitmask = lambda x : np.array([np.bool(int(c)) for c in get_bin(x,64)])
+    
+        matrix = np.array(["." for i in range(64)])
+        black = toBitmask(self.board["bl"])
+        white = toBitmask(self.board["wh"])
+        
+        for key, value in self.board.items():
+            if key in ["wh", "bl"]:
+                continue
+        
+            mask = toBitmask(value)
+            
+            matrix[np.bitwise_and(mask, black)] = key.lower()
+            matrix[np.bitwise_and(mask, white)] = key.upper()
+        
+        s = ""
+        for i in range(len(matrix)):
+            s += matrix[i]
+            if (i+1)%8==0:
+                s += "\n"
+        
+        return s
+        
+    def __str__(self):
+        return self.__repr__()
+        
 """
 main function
 is only called if you directly execute this code, not just if you import it
 """
 if __name__ == "__main__": 
     b = Board()
-    sample = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-    s = b.scan(sample)
-    print(s)
-    print(b.checkFigures(s[:8]))
+    #sample = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+    #s = b.scan(sample)
+    #print(s)
+    #print(b.checkFigures(s[:8]))
+    
+    # default string representation
+    b.board["r"] = np.uint64(int("10000001" + ("0"*8*6) + "10000001",2))
+    b.board["n"] = np.uint64(int("01000010" + ("0"*8*6) + "01000010",2))
+    b.board["b"] = np.uint64(int("00100100" + ("0"*8*6) + "00100100",2))
+    b.board["q"] = np.uint64(int("00010000" + ("0"*8*6) + "00010000",2))
+    b.board["k"] = np.uint64(int("00001000" + ("0"*8*6) + "00001000",2))
+    b.board["p"] = np.uint64(int("0"*8 + "1"*8 + ("0"*8*4) + "1"*8 + "0"*8,2))
+    
+    b.board["wh"] = np.uint64(int(("0"*8*6) + "1"*16,2))
+    b.board["bl"] = np.uint64(int("1"*16 + ("0"*8*6),2))
+    print(b)
