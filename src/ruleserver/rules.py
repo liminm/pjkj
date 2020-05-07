b759682071ac9eb1cca35a44cf8c710a53a0acd0
Ronja Froelian <ronja.froelian@campus.tu-berlin.de>
2020-05-07 17:19:14 +0200

    TODOs siehe ende vom code momentaner stand nicht auf funktionalität getestet

--- src/ruleserver/rules.py
+++ src/ruleserver/rules.py
@@ -7,7 +7,7 @@ Created on Sun May  3 14:47:31 2020
 """
 import bitboard
 import valid_move_check as vm
-import racing_kings_check_check as rk_win
+#import racing_kings_check_check as rk_win
 #import test
 
 class racingkings():
@@ -21,7 +21,6 @@ class racingkings():
     def __init__(self, FEN = None, gameTime = None, turnTime = None):
     
         self.curPlayer = '' 
-        self.maxTime = 120000
         self.turnTime = 60000
         self.playerA = ''
         
@@ -42,29 +41,18 @@ class racingkings():
         setups the Last and current boardstate, current board state is saved in self.board last board is save in self.lastBoard
         we also set current player
         """
-        if type(history) == str :
-            try:
-                self.lastBoard = self.board.parse(history)
-                parts = self.board.scan(history)
-                self.curPlayer = parts[9]
-            except:
-                return "failed to parse the FEN"
-        elif type(history) == dict:
-            try:
-                self.lastBoard = self.board.parse(history['history'][-1]['FEN'])
-                parts = self.board.scan(history['history'][-1]['FEN'])
-                self.curPlayer = parts[9]
-            except:
-                return "failed to parse the FEN"
-        else:
-                self.lastBoard = self.board.parse(self.start)
-                parts = self.board.scan(self.start)
-                self.curPlayer = parts[9]
         
-        self.board.moveUCI(uci[:2],uci[2:4])
-  
+        try:
+            self.lastBoard = bitboard.Board(history)
+            self.curPlayer = self.lastBoard.palyer
+        except:
+            return 'F'
+        try:
+            self.board.moveUCI(uci[:2],uci[2:4])
+        except:
+            return 'U'
         
-    def fenStateCheck(self,FEN, history):
+    def fenStateCheck(self,state):
         """ 
         This will be only called at the beginning of a game and report if the enterd FEN string is valid
         FEN --> The FEN string wil be cheked for format and if there is at least two kings on the field
@@ -73,12 +61,9 @@ class racingkings():
         Returns a bool if it was valid and a String telling why it wasnt
         """
         valid = True
-        con = 'V'
-        condition = {
-                'F' : " The FEN String is not acaptable, to many/less fields or diffrent figures",
-                'E': "We cant play on an Empty board need at least two kings",
-                'V': None
-                }
+        FEN = state['FEN']
+        con = None
+        ret = None
         
         #check for valid FEN
         try:
@@ -86,83 +71,138 @@ class racingkings():
         except:
             con = 'F'
             valid = False
+               
+         #check for timebudget
+        if (state['timeBudgets']['playerA'] <= 0) or (state['timeBudgets']['playerB'] <= 0):
+            valid = False
+            con = 'B'
+            ret = 'timeBudget'
+        
+        
+        # with given data we cant check for timeout and do we need to do this ? 
         #check for timeout
-        pass
-        #check for timebudget
-        pass
+        #if (self.turnTime - state['timeBudgets'][player]) <= 0:
+            #valid = False
+            #con = 'O'
+            #ret  = 'timeOut'
+            
+               
         # check for empty field
         if "8/8/8/8/8/8/8/8" in FEN :
             con = 'E'
             valid = False
+            
         #check if there are at least two kings
-        if con == '':
+        pass
+        
+        if con == None:
             self.playerA = parts[9]
-        return (valid, condition[con])
+            
+        return (valid, ret)
     
-    def moveCheck(self,uci,turnTime,history):
-        r = 'V'
-        state = None
-        winner = None
-        valid = True
-        if type(history) == str :
-             FEN = history
-        elif type(history) == dict:
-            FEN = history['history'][-1]['FEN']
+    def moveCheck(self,moveEvent,state) :
+        # pick apart the input Data
+        player = moveEvent[0]
+        
+        
+        #set beginning variables
+        r = None
+        state,winner = None
+        valid = True 
+        gameState = None
+        
+        
+        #Set FEN for easier testing
+        if type(state) == str :
+             FEN = state
+        elif type(state) == dict:
+            FEN = state['FEN']
         else:
-            FEN = ''
+            FEN = None
     
         reason = {
                 'U': "Invalid UCI string, values have to be between(inclusive) a-h and 1-8. with the format being i.e. e1g3",
                 'M': "Could not move the figure in this way",
                 'K': "Turn would put any king in checkmate",
                 'H': "The old FEN String seems to be wrong",
-                'V': None    
+                'V': None,
+                'B': " The timebudget run out",
+                'O': " You took to long for your turn"
                 }
     
         # calling all the classes
         vmc = vm.ValidCheck()
-        wc = rk_win.KingIsAttackedCheck
+        #wc = rk_win.KingIsAttackedCheck
+        
         #check for valid uci
-        if not self.validUCI(uci):
+        if not self.validUCI(moveEvent[1][0]):
            r = 'U'
            valid = False
            
         ####### Setup the turns ######
-        if self.setup(uci,history) != None:
+        r = self.setup(moveEvent[1][0],FEN)
+        if r is not None : 
+            valid = False
+        
+        #check for timebudget
+        if (state['timeBudgets'][player]-moveEvent[1][1] ) <= 0:
+            valid = False
+            r = 'B'
+            state = 'timeBudget'
+            if player == 'playerA':
+                winner = 'playerB'
+            else:
+                winner = 'playerA'
+        else:
+            state['timeBudgets'][player] = state['timeBudgets'][player]-moveEvent[1][1]
+        
+        #check for timeout
+        if (self.turnTime - state['timeBudgets'][player]) <= 0:
+            valid = False
+            r = 'O'
+            state = 'timeOut'
+            if player == 'playerA':
+                winner = 'playerB'
+            else:
+                winner = 'playerA'
+                
+         # for check valid  movement
+        try:
+            if not vmc.check(repr(self.lastBoard),repr(self.board)) :
+                valid = False
+                r = 'M'
+        except:
             valid = False
             r = 'H'
+            
         
-        #check for timeoout
-        pass
-        #check for timebudget
-        pass
-        #check if there is a figure
-        pass
-        #check if opponents figure moved
-        pass
-        """
-        # for check valid  movement
-        pos = vmc.calc_positions(self.board,self.lastBoard)
-        if not vmc.check_valid_move(pos[0],pos[1][0],pos[1][1],pos[2][0],pos[2][1]) :
-            valid = False
-            r = 'M'
         #check for checkmate
         king = self.curBoard[self.curPlayer]&self.board['k']
         moves = wc.calc_movesboard(wc.set_occupied_pos,)
         if not wc.king_is_attacked(king,wc.calc_movesboard()):
             valid = False
             r = 'K'
-        """
+        
+        
         #check for winning conditions
         pass
+    
         # everything is good
-        if r == 'V':
-            self.board.halfRounds = 1
+        
+        if r is None :
+        #TODO not quit right yet
+            self.board.halfRounds += 1
+        
             if self.curPlayer == self.playerA :
                 self.board.roundCount += 1
-                FEN = repr(self.board)
-        
-        return (valid,FEN,state,winner,reason[r])
+                
+            #set the game state and other returns
+            gameState = {
+                    'type': state,
+                    'winner': winner}
+            moveEvent['details']['postFen'],state['FEN'] = repr(self.board)
+            state['winner']  = winner
+        return (valid,gameState,reason[r])
            
           
     def validUCI(self,uci):
@@ -178,13 +218,13 @@ class racingkings():
         else :
             ret = False
         return ret
-        
+
+            
         
     
 
-rc = racingkings()
-print(rc.moveCheck('e7e8',1,"8/8/8/8/8/8/krbnKRBN/qrbnQRBN w - - 0 1"))
-#TODO Timemanagement
-#TODO how to set starting player so player movement works out
+
+#TODO checkmate einbindung
 #TODO exceptions bitbaord
-#TODO round counts increment 
+#TODO check if valid move checks for own figure
+#ToDo check if valid moves checks for any figure
