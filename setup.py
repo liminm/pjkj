5f2db25ddf2fdda624fe5ee36537e99998fe1e30
Oskar <oskar@winkels.berlin>
2020-05-18 14:44:53 +0200

    Add more info to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
-from setuptools import setup
+import setuptools
 
-setup(
-  name="pjki-server",
-  version="0.1.0",
-  author="2020 AOT PJKI Course @ TU Berlin",
-  author_email="winkels@campus.tu-berlin.de",
-  packages=["pjki-server"],
-  url="https://gitlab.tubit.tu-berlin.de/PJ-KI/server",
-  description="Gameserver for the 2020 AOT AI Tournament at TU Berlin",
-  install_requires=[
-    "flask",
-  ]
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="pjki-server",
+	version="0.1.0",
+	author="2020 AOT PJKI Course @ TU Berlin",
+	author_email="winkels@campus.tu-berlin.de",
+	description="Gameserver for the 2020 AOT AI Tournament at TU Berlin",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://gitlab.tubit.tu-berlin.de/PJ-KI/server",
+	packages=setuptools.find_packages(),
+	install_requires=[
+		"flask",
+	],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
 )
