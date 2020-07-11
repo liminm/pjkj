0c7c5bef937cfafe97c2aea15108225f50085d66
Oskar <winkels@campus.tu-berlin.de>
2020-07-11 11:45:05 +0200

    Update schema validation with tags field

--- pjkiserver/schemas.py
+++ pjkiserver/schemas.py
@@ -62,6 +62,16 @@ game = {
 		"_type": str,
 		"_re": r"^(jumpSturdy|racingKings)$",
 	},
+	"tags": {
+		"_type": list,
+		"_maxLen": 8,
+		"_elements": {
+			"_type": str,
+			"_re": r"^\w+$",
+			"_minLen": 4,
+			"_maxLen": 32,
+		}
+	},
 	"players": {
 		"_type": dict,
 		"playerA": gamePlayer,
